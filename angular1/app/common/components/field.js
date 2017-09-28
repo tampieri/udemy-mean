ca77507fe8f122872c52b19d7f635dcc51d02f97
tampieri <higortampieri@gmail.com>
2017-09-27 22:57:26 -0300

    commit 2

--- angular1/app/common/components/field.js
+++ angular1/app/common/components/field.js
@@ -6,6 +6,7 @@
       grid: '@',
       placeholder: '@',
       type: '@',
+      model: '=',
     },
     controller:[
       'gridSystem',
@@ -17,8 +18,8 @@
     <div class="{{ $ctrl.gridClasses }}">
       <div class="form-group">
         <label for="{{ $ctrl.id }}">{{ $ctrl.label }}</label>
-        <input id="{{ $ctrl.id }}" type="text" class="form-control" placeholder="{{ $ctrl.placeholder }}"
-         type="{{ $ctrl.type }}"/>
+        <input id="{{ $ctrl.id }}" class="form-control" placeholder="{{ $ctrl.placeholder }}"
+         type="{{ $ctrl.type }}" ng-model="$ctrl.model" />
       </div>
     </div>
     `
