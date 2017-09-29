4b989e8a265efddd6f2b402b0bee1c54e7953cff
tampieri <higortampieri@gmail.com>
2017-09-28 23:54:14 -0300

    commit 20170928

--- angular1/app/billingCycle/billingCycleController.js
+++ angular1/app/billingCycle/billingCycleController.js
@@ -1,18 +1,30 @@
 (function(){
   angular.module('primeiraApp').controller('BillingCycleCtrl', [
     '$http',
+    'msgs',
     BillingCycleController
   ])
 
-  function BillingCycleController($http) {
+  function BillingCycleController($http, msgs) {
     const vm = this
+    const url = 'http://localhost:3003/api/billingCycle'
+
+    vm.refresh = function(){
+        $http.get(url).then(function (response) {
+          vm.billingCycle = {}
+          vm.billingCycles = response
+        })
+    }
 
     vm.create = function(){
-      const url = 'http://localhost:3003/api/billingCycles'
-      $http.post(url, vm.billingCycle).success(function (response) {
-        vm.billingCycle = {}
-        console.log('Sucesso!')
+      $http.post(url, vm.billingCycle).then(function (response) {
+        vm.refresh()
+        msgs.addSuccess('Operação realizada com sucesso!!')
+      }).catch(function(data){
+        msgs.addError(data.errors)
       })
     }
+
+    vm.refresh()
   }
 })()
