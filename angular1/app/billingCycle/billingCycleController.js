9d18bd6a371f52fb60176a18d7bc90d8ac484c9d
tampieri <higortampieri@gmail.com>
2017-10-01 23:39:57 -0300

    commit 20171001 2

--- angular1/app/billingCycle/billingCycleController.js
+++ angular1/app/billingCycle/billingCycleController.js
@@ -1,20 +1,28 @@
 (function(){
   angular.module('primeiraApp').controller('BillingCycleCtrl', [
     '$http',
+    '$location',
     'msgs',
     'tabs',
     BillingCycleController
   ])
 
-  function BillingCycleController($http, msgs, tabs) {
+  function BillingCycleController($http, $location, msgs, tabs) {
     const vm = this
     const url = 'http://localhost:3003/api/billingCycles'
 
     vm.refresh = function(){
-        $http.get(url).then(function(response) {
+        const page = parseInt($location.search().page) || 1
+        $http.get(`${url}?skip=${(page - 1) * 10}&limit=10`).then(function(response) {
           vm.billingCycle = {credits:[{}], debts:[{}]}
           vm.billingCycles = response.data
-          tabs.show(vm, {tabList: true, tabCreate: true})
+          vm.calculateValues()
+
+
+          $http.get(`${url}/count`).then(function(response) {
+            vm.pages = Math.ceil(response.data.value / 10)
+            tabs.show(vm, {tabList: true, tabCreate: true})
+          })
         })
     }
 
@@ -29,11 +37,13 @@
 
     vm.showTabUpdate = function(billingCycle){
       vm.billingCycle = billingCycle
+      vm.calculateValues()
       tabs.show(vm, {tabUpdate: true})
     }
 
     vm.showTabDelete = function(billingCycle){
       vm.billingCycle = billingCycle
+      vm.calculateValues()
       tabs.show(vm, {tabDelete: true})
     }
 
@@ -88,6 +98,24 @@
         vm.calculateValues()
       }
     }
+
+    vm.calculateValues = function (index) {
+      vm.credit = 0
+      vm.debt =0
+
+      if (vm.billingCycle){
+        vm.billingCycle.credits.forEach(function({value}) {
+          vm.credit += !value || isNaN(value) ? 0 : parseFloat(value)
+        })
+
+        vm.billingCycle.debts.forEach(function({value}) {
+          vm.debt += !value || isNaN(value) ? 0 : parseFloat(value)
+        })
+      }
+
+      vm.total = vm.credit - vm.debt
+    }
+
 
     vm.refresh()
   }
--- angular1/app/config/routes.js
+++ angular1/app/config/routes.js
@@ -7,7 +7,7 @@
         url: "/dashboard",
         templateUrl: "dashboard/dashboard.html"
       }).state('billingCycle', {
-        url: "/billingCycles",
+        url: "/billingCycles?page",
         templateUrl: "billingCycle/tabs.html"
       })
       $urlRouterProvider.otherwise('/dashboard')
